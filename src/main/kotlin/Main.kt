bf58a049545429a789c95270c6993cba35c9aed9
twake1234 <twake.edu@gmail.com>
2024-01-03 12:37:17 +0600

    arithmatic operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,32 @@
+//fun main(args: Array<String>) {
+//    val x:Int = 10
+//    val y:Long = 1525658658656565
+//    val z:Float =22.01f
+//    val m:Double =20.25
+//    val status:Boolean = true
+//    val c:Char = 'A'
+//
+//    val firstName = "Mawahibur"
+//    val lastName = "Twake"
+//
+//    println("My name is $firstName $lastName")
+//}
+
+//logical or Arithmatic Operator
+
 fun main(args: Array<String>) {
     val x:Int = 10
-    val y:Long = 1525658658656565
-    val z:Float =22.01f
-    val m:Double =20.25
-    val status:Boolean = true
-    val c:Char = 'A'
+    val y:Int = 20
 
-    val firstName = "Mawahibur"
-    val lastName = "Twake"
+    println("The sum of $x and $y is ${x + y}")
 
-    println("My name is $firstName $lastName")
+    val age1:Int = 30
+    val age2:Int = 50
+    println(age1 > age2)
+    println(age1 < age2)
+    println(age1 == age2)
+    println(age1 != age2)
+    println(age1 <= age2)
+    println(age1 >= age2)
+
 }
