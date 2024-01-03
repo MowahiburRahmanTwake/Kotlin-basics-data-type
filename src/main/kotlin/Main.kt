d7fde8a516b71da52f56bd713db092b166d018e1
twake1234 <twake.edu@gmail.com>
2024-01-03 12:14:35 +0600

    print String

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val x:Int = 10
+    val y:Long = 1525658658656565
+    val z:Float =22.01f
+    val m:Double =20.25
+    val status:Boolean = true
+    val c:Char = 'A'
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val firstName = "Mawahibur"
+    val lastName = "Twake"
+
+    println("My name is $firstName $lastName")
 }
